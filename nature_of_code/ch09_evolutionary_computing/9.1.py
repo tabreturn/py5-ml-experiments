99bf4330abc9aba4bca313605ec874e28d5717bd
tabreturn <github@tabreturn.com>
2025-12-18 22:02:51 +1100

    - wip

--- nature_of_code/ch09_evolutionary_computing/9.1.py
+++ nature_of_code/ch09_evolutionary_computing/9.1.py
@@ -1,8 +1,8 @@
 # https://natureofcode.com/genetic-algorithms/#coding-the-genetic-algorithm
 
 population = []  # A list for the population of elements
-target = "to be or not to be"
-matingpool = []  # Start with an empty mating pool.
+target = "cat" #"to be or not to be"
+
 
 class DNA:
     """Represents a DNA sequence of random characters."""
@@ -26,40 +26,55 @@ class DNA:
         self.fitness = score / len(target)
 
     def random_character(self) -> str:
-        """Return a random character (letter, number, symbol, space,
-        and so forth).
-        """
+        """Return a random character (letter, number, symbol, space, and so forth)."""
         c = floor(random(32, 127))
         return chr(c)
 
 
 def setup():
     """Initialize each element of the population;
-    100 and 18 are hardcoded for now as the population size and length
-    of the genes array.
-    """
+    100 and 18 are hardcoded for now as the population size and length of the genes array."""
     global population
 
-    for _ in range(100):
-        population.append(DNA(18))
+#     for _ in range(100):
+#         population.append(DNA(18))
+    population.append(DNA(3))
+    population.append(DNA(3))
+    population.append(DNA(3))
+    population[0].genes = list('cat')
+    population[1].genes = list('gag')
 
+
+def draw():
     for phrase in population:
         phrase.calculate_fitness(target)
 
+    matingpool = []  # Start with an empty mating pool.
+
     for phrase in population:
-        """n is equal to fitness times 100. 100 is an arbitrary way to
-        scale the percentage of fitness to a larger integer value.
-        """
+        """n is equal to fitness times 100. 100 is an arbitrary way to scale the
+        percentage of fitness to a larger integer value."""
+
         n = floor(phrase.fitness * 100)
-        print(n)
+        
+        print(phrase.genes); print(n)
 
-#         for _ in len(n):
-#             """Add each member of the population to the mating pool n
-#             times.
-#             """
-#             matingpool.append(phrase)
+        for _ in range(n):
+            """Add each member of the population to the mating pool n times."""
+            matingpool.append(phrase)
+
+    for m in matingpool:
+        print(m.genes)
 
 
-#def draw():
-#    for phrase in population:
-#        phrase.calculate_fitness(target)
+
+
+
+
+
+    no_loop()
+
+def key_pressed():
+    if key == 'x':
+        exit_sketch()
+    

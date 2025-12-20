9d0a2261047e04717136041cadaab40f2fbafeb6
tabreturn <github@tabreturn.com>
2025-12-20 21:28:16 +1100

    - wip

--- nature_of_code/ch09_evolutionary_computing/9.1.py
+++ nature_of_code/ch09_evolutionary_computing/9.1.py
@@ -1,17 +1,18 @@
 # https://natureofcode.com/genetic-algorithms/#coding-the-genetic-algorithm
 
 from dna import DNA
-from population import Population
+#from population import Population
 
-mutationrate = 0.01            # Mutation rate
-populationsize = 150           # Population size
+mutationrate = 0.1            # Mutation rate
+populationsize = 5           # Population size
 population = []                # Population list
-target = "to be or not to be"  # Target phrase
+target = 'cat'  # Target phrase
 
 
 def setup():
-    global population
+    global monospace, population
     size(640, 360)
+    monospace = create_font('DejaVu Sans Mono', 32)
 
     # Step 1: Initialization
     for _ in range(populationsize):
@@ -22,6 +23,9 @@ def draw():  # Step 2: Selection
 
     for phrase in population:  # Step 2a: Calculate fitness.
         phrase.calculate_fitness(target)
+    
+    print('population:')
+    print(*(dna.genes for dna in population), sep="\n")
 
     matingpool = []  # Step 2b: Build the mating pool.
 
@@ -30,7 +34,11 @@ def draw():  # Step 2: Selection
         n = floor(phrase.fitness * 100)
         for _ in range(n):
             matingpool.append(phrase)
-
+        # Add one of each, even if score is zero (so always enough to mate).
+        matingpool.append(phrase)
+    
+    print(f'total mating pool: {len(matingpool)}')
+    
     for i, _ in enumerate(population):  # Step 3: Reproduction
         parenta = random_choice(matingpool)
         parentb = random_choice(matingpool)
@@ -44,32 +52,38 @@ def draw():  # Step 2: Selection
         population[i] = child
 
         # Step 4: Repetition. Go back to the beginning of draw()!
-
-
-
     
+    print(f'parents: \n{parenta.genes}\n{parentb.genes}')
 
     background(255)
     fill(0)
-    #text_font('Courier')
+    text_font(monospace)
     text_size(12)
     text('Best phrase:', 10, 32)
     text_size(24)
     text('answer', 10, 64)
-#     let statstext =
-#     'total generations:     ' + population.getGenerations() + '\n'
-#     statstext +=
-#     'average fitness:       ' + nf(population.getAverageFitness(), 0, 2) + '\n'
-#     statstext += 'total population:      ' + popmax + '\n'
-#     statstext += 'mutation rate:         ' + floor(mutationRate * 100) + '%'
+    
+    statstext = (
+      f'total generations:     {frame_count}\n'
+      f'average fitness:       nf(population.getAverageFitness(), 0, 2)\n'
+      f'total population:      {populationsize}\n'
+      f'mutation rate:         {floor(mutationrate * 100)}%'
+    )
+
 # 
-#     textSize(12)
-#     text(statstext, 10, 96)
-#     textSize(8)
-#     text(population.allPhrases(), width / 2, 24)
+    text_size(12)
+    text(statstext, 10, 96)
+#    textSize(8)
+#    text(population.allPhrases(), width / 2, 24)
+
+    print(f'frame count: {frame_count}')
+    no_loop()
 
 
 
 def key_pressed():
     if key == 'x':
         exit_sketch()
+    if key == 'z':
+        loop()
+        
